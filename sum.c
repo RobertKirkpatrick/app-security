c8fb3e12960ca15a01947e204cfb69f62ec39abb
robertkirkpatrick <rk3505@nyu.edu>
2019-09-07 18:39:11 -0400

    testing

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   int a, b, c;
+   printf("Enter two numbers to add, separated by a space: ");
+   scanf("%d%d",&a,&b);
+   c = a + b;
+   printf("The sum of equals %d\n",c);
+   return 0;
+}
