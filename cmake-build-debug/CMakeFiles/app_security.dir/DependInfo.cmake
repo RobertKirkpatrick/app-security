6cda93127f091a0822b12b8a1cb2317e2336ed70
RobertKirkpatrick <rk3505@nyu.edu>
2019-09-29 19:46:31 -0400

    created spell.c based on my functiontesting effort. Trying to get it to make with the provided makefile
    Signed-off-by: RobertKirkpatrick <rk3505@nyu.edu>

---
+++ cmake-build-debug/CMakeFiles/app_security.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rob/appsecurity/app-security/AppSecAssignment1/dictionary.c" "/home/rob/appsecurity/app-security/cmake-build-debug/CMakeFiles/app_security.dir/AppSecAssignment1/dictionary.c.o"
+  "/home/rob/appsecurity/app-security/AppSecAssignment1/test_main.c" "/home/rob/appsecurity/app-security/cmake-build-debug/CMakeFiles/app_security.dir/AppSecAssignment1/test_main.c.o"
+  "/home/rob/appsecurity/app-security/dictionary.c" "/home/rob/appsecurity/app-security/cmake-build-debug/CMakeFiles/app_security.dir/dictionary.c.o"
+  "/home/rob/appsecurity/app-security/functiontest/main.c" "/home/rob/appsecurity/app-security/cmake-build-debug/CMakeFiles/app_security.dir/functiontest/main.c.o"
+  "/home/rob/appsecurity/app-security/spell.c" "/home/rob/appsecurity/app-security/cmake-build-debug/CMakeFiles/app_security.dir/spell.c.o"
+  "/home/rob/appsecurity/app-security/sum.c" "/home/rob/appsecurity/app-security/cmake-build-debug/CMakeFiles/app_security.dir/sum.c.o"
+  "/home/rob/appsecurity/app-security/test_main.c" "/home/rob/appsecurity/app-security/cmake-build-debug/CMakeFiles/app_security.dir/test_main.c.o"
+  "/home/rob/appsecurity/app-security/valgrind/valgrindtest.c" "/home/rob/appsecurity/app-security/cmake-build-debug/CMakeFiles/app_security.dir/valgrind/valgrindtest.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../AppSecAssignment1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/app_security.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app_security.dir/AppSecAssignment1/dictionary.c.o"
+  "CMakeFiles/app_security.dir/AppSecAssignment1/test_main.c.o"
+  "CMakeFiles/app_security.dir/dictionary.c.o"
+  "CMakeFiles/app_security.dir/functiontest/main.c.o"
+  "CMakeFiles/app_security.dir/spell.c.o"
+  "CMakeFiles/app_security.dir/sum.c.o"
+  "CMakeFiles/app_security.dir/test_main.c.o"
+  "CMakeFiles/app_security.dir/valgrind/valgrindtest.c.o"
+  "app_security"
+  "app_security.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/app_security.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
