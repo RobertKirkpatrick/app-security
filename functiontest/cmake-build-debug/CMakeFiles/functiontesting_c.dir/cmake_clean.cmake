04e4ad457794130567460de3e79ba73e5385bd57
RobertKirkpatrick <rk3505@nyu.edu>
2019-09-29 17:07:31 -0400

    add functiontesting folder that was not in the repo. This has 3 functions to be moved to spell.c
    Signed-off-by: RobertKirkpatrick <rk3505@nyu.edu>

---
+++ functiontest/cmake-build-debug/CMakeFiles/functiontesting_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/functiontesting_c.dir/main.c.o"
+  "functiontesting_c"
+  "functiontesting_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/functiontesting_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
